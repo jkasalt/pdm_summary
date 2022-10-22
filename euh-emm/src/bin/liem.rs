fc7706cd9d623fbe83e31576f84102d1ca09b038
Luca Bracone <lucabr123@gmail.com>
2022-10-22 18:13:55 +0200

    I think it works now

--- euh-emm/src/bin/liem.rs
+++ euh-emm/src/bin/liem.rs
@@ -111,7 +111,7 @@ type BMatrix<const D: usize> = SMatrix<bool, D, D>;
 // }
 
 struct EStep<const D: usize> {
-    // p: Matrix<D>,
+    p: Matrix<D>,
     d: Matrix<D>,
 }
 
@@ -124,78 +124,94 @@ impl<const D: usize> EStep<D> {
         }));
         let d: Matrix<D> = p.map(|p_jk| (1.0 - p_jk) / v0.powi(2) + p_jk / v1.powi(2));
 
-        Self { d }
+        Self { p, d }
     }
 }
 
-struct CMStep {
+struct CMStep<const D: usize> {
     alpha: f64,
     beta: f64,
     lambda: f64,
-    pi: f64,
+    s: Matrix<D>,
     v0: f64,
     v1: f64,
 }
 
-impl CMStep {
-    fn new(alpha: f64, beta: f64, lambda: f64, pi: f64, v0: f64, v1: f64) -> Self {
+impl<const D: usize> CMStep<D> {
+    fn new<const N: usize>(
+        alpha: f64,
+        beta: f64,
+        lambda: f64,
+        v0: f64,
+        v1: f64,
+        x: &SMatrix<f64, N, D>,
+    ) -> Self {
+        let s = x.tr_mul(x);
         CMStep {
+            s,
             alpha,
             beta,
             lambda,
-            pi,
             v0,
             v1,
         }
     }
 
-    fn compute<const D: usize, const N: usize>(
-        &self,
-        x: &SMatrix<f64, N, D>,
-        z: &BMatrix<D>,
-        a: &Matrix<D>,
-    ) -> (f64, Matrix<D>)
+    fn compute(&self, pi: f64, a: &Matrix<D>, n: usize) -> (f64, Matrix<D>)
     where
         [(); D - 1]:,
     {
-        let p = D;
-        let n = N as f64;
-        let s_z = z.map(|z_ij| z_ij as i32).sum() as f64;
-        let new_pi =
-            (self.alpha + s_z - 1.0) / (self.alpha + self.beta + (p * (p - 1) / 2) as f64 - 2.0);
-        let s = x.tr_mul(x);
-        let d = EStep::compute(a, self.pi, self.v0, self.v1).d;
-
-        println!("d: {d}");
-
+        let mut s = self.s.clone_owned();
+        let e_step = EStep::compute(a, pi, self.v0, self.v1);
+        let sum_delta = e_step.p.upper_triangle().sum();
+        let new_pi = (self.alpha + dbg!(sum_delta) - 1.0)
+            / (self.alpha + self.beta + (D * (D - 1) / 2) as f64 - 2.0);
+        let mut d = e_step.d;
         let mut omega = a.clone_owned();
-        let s_22 = s.get((D - 1, D - 1)).unwrap();
-        let s_12 = s.index((0..D - 1, D - 1));
-        let d_12_inv = Matrix::<{ D - 1 }>::from_diagonal(&d.fixed_slice(0, D - 1))
-            .try_inverse()
-            .unwrap();
-        let mut new_omega = Matrix::<D>::zeros();
 
         for t in 0..D {
+            // Swap columns and calculate components based off original omega
             omega.swap_columns(t, D - 1);
+            omega.swap_rows(t, D - 1);
+            d.swap_columns(t, D - 1);
+            d.swap_rows(t, D - 1);
+            s.swap_columns(t, D - 1);
+            s.swap_rows(t, D - 1);
+
+            // Get relevant values
+            let s_22 = s.get((D - 1, D - 1)).unwrap();
+            let s_12 = s.slice((0, D - 1), (D - 1, 1));
+            let d_12 = d.fixed_slice(0, D - 1);
+
             let omega_11_inv = omega.slice((0, 0), (D - 1, D - 1)).try_inverse().unwrap();
-            let c: Matrix<{ D - 1 }> = (s_22 + self.lambda) * omega_11_inv.clone() + d_12_inv;
+            let c: Matrix<{ D - 1 }> =
+                (s_22 + self.lambda) * omega_11_inv.clone() + Matrix::from_diagonal(&d_12);
 
-            // update column upper part
-            let omega_12_lp1 = c.try_inverse().unwrap() * s_12;
-            new_omega
-                .slice_mut((0, t), (D - 1, 1))
+            // Update column upper part
+            let omega_12_lp1 = -c.try_inverse().unwrap() * s_12;
+            omega
+                .slice_mut((0, D - 1), (D - 1, 1))
                 .copy_from(&omega_12_lp1);
+            omega
+                .slice_mut((D - 1, 0), (1, D - 1))
+                .copy_from(&omega_12_lp1.transpose());
 
-            // update column lower part
+            // Update column diagonal bit
             let omega_22_lp1 = (omega_12_lp1.tr_mul(&omega_11_inv) * omega_12_lp1)
-                .add_scalar(n / (self.lambda + s_22));
-            new_omega
-                .slice_mut((D - 1, t), (1, 1))
+                .add_scalar(n as f64 / (self.lambda + s_22));
+            omega
+                .slice_mut((D - 1, D - 1), (1, 1))
                 .copy_from(&omega_22_lp1);
+
+            // Swap back column, so that we are ready for next iteration
             omega.swap_columns(t, D - 1);
+            omega.swap_rows(t, D - 1);
+            d.swap_columns(t, D - 1);
+            d.swap_rows(t, D - 1);
+            s.swap_columns(t, D - 1);
+            s.swap_rows(t, D - 1);
         }
-        (new_pi, new_omega)
+        (new_pi, omega)
     }
 }
 
@@ -225,8 +241,8 @@ impl<const D: usize> Distribution<SVector<f64, D>> for MultivariateNormal<D> {
 }
 
 const M_SIZE: usize = 5;
-const SAMPLE_SIZE: usize = 100;
-const NUM_STEPS: usize = 4;
+const SAMPLE_SIZE: usize = 1000;
+const NUM_STEPS: usize = 100;
 
 fn main() {
     let mut ar2 = Matrix::<M_SIZE>::zeros();
@@ -255,23 +271,20 @@ fn main() {
             },
         );
 
-    let z = ar2.map(|a_ij| rng.gen_bool(a_ij));
-    // dbg!(&x);
-    // dbg!(&z);
-
     let alpha = 1.0;
     let beta = 1.0;
     let lambda = 1.0;
     let mut pi = 0.5;
-    let v0 = 1.0;
-    let v1 = 100.0;
+    let v0 = 0.1;
+    let v1 = 10.0;
 
     for _ in 0..NUM_STEPS {
         // println!("{omega}");
-        (pi, ar2) = CMStep::new(alpha, beta, lambda, pi, v0, v1).compute(&x, &z, &ar2);
-        println!("pi: {pi}, omega: {ar2}");
+        (pi, ar2) = CMStep::new(alpha, beta, lambda, v0, v1, &x).compute(pi, &ar2, SAMPLE_SIZE);
+        // println!("pi: {pi}, omega: {ar2}");
     }
-    println!("{ar2}");
+    println!("{pi}");
+    println!("{ar2:?}");
 }
 
 /// An error enum for the methods in this file
