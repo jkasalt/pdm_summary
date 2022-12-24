8f706db8200c10c2d59270cbdbb2dd6d1841fad7
Luca Bracone <lucabr123@gmail.com>
2022-12-24 17:55:03 +0100

    Q function, bugfixes, plotting true/false pos/neg

--- r-bmg/bmg.r
+++ r-bmg/bmg.r
@@ -6,13 +6,13 @@ library(testit)
 library(matrixcalc)
 library(LaplacesDemon)
 
-K <- 2
+K <- 4
 
 hyperparams <- list(
-  "v0" = 1.0,
+  "v0" = 0.08,
   "v1" = 10,
   "lambda" = rep(2, K),
-  "n" = c(50, 200),
+  "n" = c(50, 50, 50, 50),
   "p" = 20
 )
 
@@ -27,43 +27,77 @@ bmg.ecm <- function(y, Omega, theta, Sigma, params, maxiter = 30) {
     S[[k]] <- t(y[[k]]) %*% y[[k]]
   }
 
+  log_liks <- c()
   # Run ECM
   for (t in 1:maxiter) {
     updated_vals <- bmg.mstep(Omega, theta, Sigma, S, params)
     Omega <- updated_vals$Omega
     theta <- updated_vals$theta
+    
+    # Get posterior likelihood
+    log_lik <- bmg.posterior_likelihood(y, Omega, theta, Sigma, params)
+    log_liks <- append(log_liks, log_lik)
   }
 
   # Get posterior edge inclusion probabilities
   estep <- bmg.estep(Omega, theta, params)
 
   # TODO: q is not posterior edge inclusion!
-  return(list("theta" = theta, "Omega" = Omega, "prob" = estep$prob))
+  return(list("theta" = theta, "Omega" = Omega, "prob" = estep$prob, "log_liks" = log_liks))
 }
 
 bmg.estep <- function(Omega, theta, params) {
   v0 <- params$v0
   v1 <- params$v1
 
-  # TODO: use log to make computation more stable
+  # TODO: maybe use log to make computation more stable
   a1 <- dnorm(Omega, mean = 0, sd = v1) * pnorm(theta)
   a0 <- dnorm(Omega, mean = 0, sd = v0) * (1 - pnorm(theta))
   pdeltais1 <- a1 / (a1 + a0)
-
-  d <- pdeltais1 / v0^2 + (1 - pdeltais1) / v1^2
+  print(sum(pdeltais1 > 0.5))
+  
+  d <- (1 - pdeltais1) / v0^2 + pdeltais1 / v1^2
 
   mr1_log <- dnorm(theta, log = TRUE) - pnorm(theta, log.p = TRUE)
   mr0_log <- dnorm(theta, log = TRUE) - pnorm(theta, log.p = TRUE, lower.tail = F)
 
   q <- theta - exp(mr0_log) + pdeltais1 * (exp(mr1_log) + exp(mr0_log))
-  apply(q, 3, function(ff) sum(ff[upper.tri(ff)]))
   return(list("d" = d, "q" = q, "prob" = pdeltais1))
 }
 
-bmg.posterior_likelihood <- function(y, Omega, theta, Sigma) {
+bmg.posterior_likelihood <- function(y, Omega, theta, Sigma, params) {
   p <- params$p
-  const <- K * p * (p-1) / 2 * log(2 * pi) - p * (p-1) / 4 * log(det(Sigma))
+  n <- params$n
+  lambda <- params$lambda
+  Sigma_inv <- solve(Sigma)
+  estep <- bmg.estep(Omega, theta, params)
+  const <- log(2 * pi) * K * p * (p-1) / 2 - log(det(Sigma)) * p * (p-1) / 4 
   
+  # Sum the terms that depend only on k
+  sum_k <- 0
+  for (k in 1:K) {
+    S_k <- t(y[[k]]) %*% y[[k]]
+    Omega_k <- Omega[,,k]
+    lambda_k <- lambda[k]
+    this_k <- 0.5 * (n[k] * log(det(Omega_k)) - sum(diag(S_k %*% Omega_k)) - lambda_k * sum(diag(Omega_k)))
+    sum_k <- sum_k + this_k
+  }
+  
+  # Sum the terms that depend only on i,j
+  sum_ij <- 0
+  for (i in 1:(p-1)) {
+    for (j in (i+1):p) {
+      this_ij <- - 0.5 * t(theta[i,j,]) %*% Sigma_inv %*% theta[i,j,]
+      sum_ij <- sum_ij + this_ij
+    }
+  }
+  
+  # Sum the terms that depend on both i,j, and k
+  omega_bit <- -0.5 * sum(Omega^2 * estep$d)
+  theta_bit <- - 0.5 * sum(theta^2 - 2 * theta * estep$q)
+  sum_ijk <-omega_bit + theta_bit
+  
+  return(sum_k + sum_ij + sum_ijk)
 }
 
 #' Calculates the next iterates for omega and theta
@@ -91,8 +125,7 @@ bmg.mstep <- function(Omega, theta, Sigma, S, params) {
     for (i in 1:p) {
       for (j in 1:p) {
         new_theta[i, j, k] <-
-          (q[i, j, k] - Sigma_inv[k, ] %*% theta[i, j, ] 
-           + Sigma_inv[k, k] * theta[i, j, k]) / (1 + Sigma_inv[k, k])
+          (q[i, j, k] - Sigma_inv[k, -k] %*% theta[i, j, -k])  / (1 + Sigma_inv[k, k])
       }
     }
 
@@ -137,6 +170,7 @@ bmg.mstep <- function(Omega, theta, Sigma, S, params) {
       d_k[c(t, p), ] <- d_k[c(p, t), ]
       S_k[c(t, p), ] <- S_k[c(p, t), ]
     }
+    Omega[, , k] <- Omega_k
   }
   return(list("theta" = new_theta, "Omega" = Omega))
 }
@@ -226,4 +260,25 @@ for (i in 1:p) {
 
 y <- lapply(g, function(gg) gg$data)
 Omega_0 <- rWishart(K, p, diag(p))
+Om0 <- Omega_0
 cm <- bmg.ecm(y, Omega_0, theta_0, Sigma, hyperparams)
+cm.estep <- bmg.estep(cm$Omega, cm$theta, hyperparams)
+
+# Plotting
+num <- 1
+xlim <- c(min(cm$Omega[,,num]), max(cm$Omega[,,num]))
+ylim <- c(min(g[[num]]$omega), max(g[[num]]$omega))
+true_positive_mask <- cm.estep$prob[,,num] > 0.5 & g[[num]]$omega > 1e-4
+false_positive_mask <- cm.estep$prob[,,num] > 0.5 & g[[num]]$omega < 1e-4
+true_negative_mask <- cm.estep$prob[,,num] < 0.5 & g[[num]]$omega < 1e-4
+false_negative_mask <- cm.estep$prob[,,num] < 0.5 & g[[num]]$omega > 1e-4
+
+mask <- true_positive_mask
+plot(cm$Omega[,,num][mask], g[[num]]$omega[mask], xlim=xlim, ylim=ylim, col = "red")
+mask <- true_negative_mask
+points(cm$Omega[,,num][mask], g[[num]]$omega[mask], col = "orange")
+mask <- false_positive_mask
+points(cm$Omega[,,num][mask], g[[num]]$omega[mask], col = "blue")
+mask <- false_negative_mask
+points(cm$Omega[,,num][mask], g[[num]]$omega[mask], col = "purple")
+legend(x="topleft", legend=c("True +", "True -", "False +", "False -"), fill = c("red", "orange", "blue", "purple"))
